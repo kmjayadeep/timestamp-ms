552626f28f9321d8ec3b180c78e64fb42b1b958d
Jayadeep KM <kmjayadeep@gmail.com>
2016-01-22 20:27:02 +0530

    complete

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,42 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/:dString', function(req, res, next) {
-	console.log(req.params.dString)
+router.get('/', function(req, res, next) {
     res.render('index', {
         title: 'Express'
     });
+});
+
+router.get('/:dString', function(req, res, next) {
+    var dString = req.params.dString
+    if (isNaN(+dString)) {
+        var date = new Date(dString)
+        if (date.getTime()) {
+            var data = {
+                unix: date.getTime(),
+                natural: date.toDateString()
+            }
+        } else
+            var data = {
+                unix: null,
+                natural: null
+            }
+    } else {
+        var date = new Date(+dString)
+        if (date.getTime()) {
+            var data = {
+                unix: +dString,
+                natural: date.toDateString() //date.getMonth()+' '+date.getDate()+', '+date.getFullYear()
+            }
+        } else
+            var data = {
+                unix: null,
+                natural: null
+            }
+    }
+
+
+    res.json(data)
 });
 
 
